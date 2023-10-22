7bb28dee0d74952a4838a8b46533ac86e6e4b6fb
Haridarshan Choudhary <68905333+hdck007@users.noreply.github.com>
2023-10-22 18:16:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("madarchod")
 const getUrl = document.currentScript.getAttribute("data-url");
 const theHtml = `<iframe src="${getUrl}" width="800px"  height="800px"></iframe>`
 document.body.insertAdjacentHTML('beforeend', theHtml);
